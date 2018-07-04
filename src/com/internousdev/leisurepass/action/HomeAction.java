276e8dbd71eeaaf4e4fffee2204014d2658c1966
XNCOAISN <end.pgm@gmail.com>
2018-07-04 17:24:12 +0900

    HomeAction作成

---
+++ src/com/internousdev/leisurepass/action/HomeAction.java
@@ -0,0 +1,71 @@
+package com.internousdev.leisurepass.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.leisurepass.dao.MCategoryDAO;
+import com.internousdev.leisurepass.dao.MPlaceDAO;
+import com.internousdev.leisurepass.dto.MCategoryDTO;
+import com.internousdev.leisurepass.dto.MPlaceDTO;
+import com.internousdev.leisurepass.util.CommonUtility;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport implements SessionAware{
+
+	private String placeId;
+	private String categoryId;
+	private Map<String, Object> session;
+
+	public String execute() {
+
+		if (!(session.containsKey("loginId")) && !(session.containsKey("tempUserId"))) {
+			// ログインしていない場合は一時的にユーザーIDをランダムで設定
+			 CommonUtility commonUtility = new CommonUtility();
+			 session.put("tempUserId", commonUtility.RandomValue());
+		}
+
+		if(!session.containsKey("logined")) {
+			// 未ログイン
+			session.put("logined", 0);
+		}
+
+		if(!session.containsKey("mPlaceList")) {
+			// 場所リストを取得
+			MPlaceDAO mPlaceDao = new MPlaceDAO();
+			List<MPlaceDTO> mPlaceDtoList = mPlaceDao.getMPlaceList();
+			session.put("mPlaceDtoList", mPlaceDtoList);
+		}
+
+		if(!session.containsKey("mCategoryList")) {
+			// カテゴリーを取得
+			MCategoryDAO mCategoryDao = new MCategoryDAO();
+			List<MCategoryDTO> mCategoryDtoList = mCategoryDao.getMCategoryList();
+			session.put("mCategoryDtoList", mCategoryDtoList);
+		}
+
+		return SUCCESS;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getPlaceId() {
+		return placeId;
+	}
+
+	public void setPlaceId(String placeId) {
+		this.placeId = placeId;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+}
