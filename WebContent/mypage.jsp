80f1c165379a6ae2ae3cd7211347053ed2c8b3b0
ykyk-314 <ykym14x3tkhr@outlook.jp>
2018-07-04 15:39:46 +0900

    mypage.jsp,ProductInfoDTO作成

---
+++ WebContent/mypage.jsp
@@ -0,0 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./css/style.css">
+		<title>マイページ</title>
+	</head>
+	<body>
+
+	<jsp:include page="header.jsp" />
+	<div id="main" class="cf">
+		<jsp:include page="navigation.jsp" />
+		<div id="contents">
+		<h1>マイページ</h1>
+			<s:form action="ParchaseHistoryAction">
+
+			<table class="vertical-list-table">
+			<tr>
+				<th scope="row"><s:label value="姓"/></th>
+				<td><s:property value="#session.familyName"/></td>
+			</tr>
+
+			<tr>
+				<th scope="row"><s:label value="名"/></th>
+				<td><s:property value="#session.firstName"/></td>
+			</tr>
+
+			<tr>
+				<th scope="row"><s:label value="ふりがな"/></th>
+				<td><s:property value="#session.familyNameKana"/><span>　</span><s:property value="#session.firstNameKana"/></td>
+			</tr>
+
+			<tr>
+				<th scope="row"><s:label value="性別"/></th>
+				<td><s:if test="#session.sex==0">男性</s:if><s:if test="#session.sex==1">女性</s:if></td>
+			</tr>
+
+			<tr>
+				<th scope="row"><s:label value="メールアドレス"/></th>
+				<td><s:property value="#session.email"/></td>
+			</tr>
+			</table>
+
+			<div class="submit_btn_box">
+				<s:submit value="購入履歴" class="submit_btn" />
+			</div>
+
+			</s:form>
+		</div>
+
+	</div>
+
+	<jsp:include page="footer.jsp" />
+
+	</body>
+</html>
---
+++ src/com/internousdev/leisurepass/dto/ProductInfoDTO.java
@@ -0,0 +1,110 @@
+package com.internousdev.leisurepass.dto;
+
+import java.util.Date;
+
+public class ProductInfoDTO {
+
+//	フィールド　製品情報テーブルより
+	private int id;
+	private int productId;
+	private String productName;
+	private String productNameKana;
+	private String productDescription;
+	private int categoryId;
+	private int price;
+	private String imageFilePath;
+	private String inamgeFileName;
+	private Date releaseDate;
+	private String releaseCompany;
+	private int status;
+	private Date registDate;
+	private Date updateDate;
+
+
+//	メソッド　ゲットセット
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductNameKana() {
+		return productNameKana;
+	}
+	public void setProductNameKana(String productNameKana) {
+		this.productNameKana = productNameKana;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+	public String getInamgeFileName() {
+		return inamgeFileName;
+	}
+	public void setInamgeFileName(String inamgeFileName) {
+		this.inamgeFileName = inamgeFileName;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getReleaseCompany() {
+		return releaseCompany;
+	}
+	public void setReleaseCompany(String releaseCompany) {
+		this.releaseCompany = releaseCompany;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getRegistDate() {
+		return registDate;
+	}
+	public void setRegistDate(Date registDate) {
+		this.registDate = registDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
