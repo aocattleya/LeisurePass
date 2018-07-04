c644fc2885b81138a9a3eabadebd43c86ab80b45
XNCOAISN <end.pgm@gmail.com>
2018-07-04 15:31:56 +0900

    MCategoryのDAO,DTO作成

---
+++ src/com/internousdev/leisurepass/dao/MCategoryDAO.java
@@ -0,0 +1,56 @@
+package com.internousdev.leisurepass.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.internousdev.leisurepass.dto.MCategoryDTO;
+import com.internousdev.leisurepass.util.DBConnector;
+
+public class MCategoryDAO {
+
+	public List<MCategoryDTO> selectAll(){
+
+		DBConnector dbConnector = new DBConnector();
+		Connection connection = dbConnector.getConnection();
+
+		List<MCategoryDTO> list = new ArrayList<MCategoryDTO>();
+
+		String sql = "SELECT * FROM m_category";
+
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			while(resultSet.next()) {
+				MCategoryDTO dto = new MCategoryDTO();
+				dto.setId(resultSet.getInt("id"));
+				dto.setCategoryId(resultSet.getInt("category_id"));
+				dto.setCategoryName(resultSet.getString("category_name"));
+				dto.setCategoryDescription(resultSet.getString("category_description"));
+				dto.setInsertDate(resultSet.getDate("insert_date"));
+				dto.setUpdateDate(resultSet.getDate("update_date"));
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		if (list.size() == 0) {
+			list = null;
+		}
+
+		return list;
+	}
+}
---
+++ src/com/internousdev/leisurepass/dto/MCategoryDTO.java
@@ -0,0 +1,61 @@
+package com.internousdev.leisurepass.dto;
+
+import java.util.Date;
+
+public class MCategoryDTO {
+
+	private int id;
+	private int categoryId;
+	private String categoryName;
+	private String categoryDescription;
+	private Date insertDate;
+	private Date updateDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getCategoryDescription() {
+		return categoryDescription;
+	}
+
+	public void setCategoryDescription(String categoryDescription) {
+		this.categoryDescription = categoryDescription;
+	}
+
+	public Date getInsertDate() {
+		return insertDate;
+	}
+
+	public void setInsertDate(Date insertDate) {
+		this.insertDate = insertDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+}
