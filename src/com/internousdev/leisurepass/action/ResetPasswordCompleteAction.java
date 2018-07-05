0743a1656da0a0eab02ded76f2da0f702dd07920
Eri-Naka <naka528719@yahoo.co.jp>
2018-07-05 10:47:30 +0900

    パスワード再設定完了Action jsp 作成

---
+++ src/com/internousdev/leisurepass/action/ResetPasswordCompleteAction.java
@@ -0,0 +1,63 @@
+package com.internousdev.leisurepass.action;
+
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.leisurepass.dao.UserInfoDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ResetPasswordCompleteAction {
+
+	private String categoryId;
+	private String loginId;
+	private String password;
+	private Map<String, Object> session;
+
+	public String execute() {
+		String result = ERROR;
+		UserInfoDAO userInfoDAO = new UserInfoDAO();
+		int count = userInfoDAO.resetPassword(String.valueOf(session.get("loginId")),
+				String.valueOf(session.get("newPassword")));
+		if (count > 0) {
+			result = SUCCESS;
+		} else {
+			result = ERROR;
+		}
+		return result;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+}
+
+/*中山　7/5*/
