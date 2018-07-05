993daae5bce2888aaed6be41fc692f77f2570bcb
XNCOAISN <end.pgm@gmail.com>
2018-07-05 11:11:46 +0900

    HomeAction更新

--- src/com/internousdev/leisurepass/action/HomeAction.java
+++ src/com/internousdev/leisurepass/action/HomeAction.java
@@ -16,6 +16,9 @@ public class HomeAction extends ActionSupport implements SessionAware{
 
 	private String placeId;
 	private String categoryId;
+	private String placeName;
+	private String categoryName;
+
 	private Map<String, Object> session;
 
 	public String execute() {
@@ -49,19 +52,43 @@ public class HomeAction extends ActionSupport implements SessionAware{
 	}
 
 	public String getCategoryId() {
+		System.out.println("getCategoryId: " + categoryId);
 		return categoryId;
 	}
 
 	public void setCategoryId(String categoryId) {
+		System.out.println("setCategoryId: " + categoryId);
 		this.categoryId = categoryId;
 	}
 
 	public String getPlaceId() {
+		System.out.println("getPlaceId: " + placeId);
 		return placeId;
 	}
 
 	public void setPlaceId(String placeId) {
+		System.out.println("setPlaceId: " + placeId);
 		this.placeId = placeId;
+	}
+
+	public String getPlaceName() {
+		System.out.println("getPlaceName: " + placeName);
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		System.out.println("setPlaceName: " + placeName);
+		this.placeName = placeName;
+	}
+
+	public String getCategoryName() {
+		System.out.println("getCategoryName: " + categoryName);
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		System.out.println("setCategoryName: " + categoryName);
+		this.categoryName = categoryName;
 	}
 
 	@Override
