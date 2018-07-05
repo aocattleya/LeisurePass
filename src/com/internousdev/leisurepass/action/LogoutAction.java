f855306be92155c0e350678c8ff43ac424f84bd4
kahleng <a097976465@gmail.com>
2018-07-05 17:38:32 +0900

    LogoutAction　修正

--- src/com/internousdev/leisurepass/action/LogoutAction.java
+++ src/com/internousdev/leisurepass/action/LogoutAction.java
@@ -4,37 +4,42 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
-//import com.internousdev.sampleweb.dao.UserInfoDAO;
+import com.internousdev.leisurepass.dao.UserInfoDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LogoutAction extends ActionSupport implements SessionAware{
+public class LogoutAction extends ActionSupport implements SessionAware {
 
 	private String categoryId;
 	private Map<String, Object> session;
+
 	public String execute() {
 		String result = ERROR;
 		UserInfoDAO userInfoDAO = new UserInfoDAO();
-		String loginId = String.valueof(session.get("loginId"));
-		//ログイン保持
-		boolean savedLogiId = Boolean.valueOf(String.valueOf(session.get("savedLoginId")));
-		int count  = userInfoDao.logout(loginId);
-		if(count > 0) {
+		String loginId = String.valueOf(session.get("loginId"));
+		// ログイン保持
+		boolean savedLoginId = Boolean.valueOf(String.valueOf(session.get("savedLoginId")));
+		int count = userInfoDAO.logout(loginId);
+		if (count > 0) {
 			session.clear();
-			session.put("savedLoginId", savedLoginId);
+			session.put("savedLoginId",savedLoginId);
 			session.put("loginId", loginId);
 			result = SUCCESS;
 		}
 		return result;
 	}
+
 	public String getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(String categoryId) {
 		this.categoryId = categoryId;
 	}
-	public Map<String, Object> getSession(){
+
+	public Map<String, Object> getSession() {
 		return session;
 	}
+
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
