2140130271c052174b1994a4e17dbf9a04bb96a5
aocattleya <aocattleya@yahoo.co.jp>
2018-07-05 12:30:27 +0900

    決済完了機能のActionコード修正

--- src/com/internousdev/leisurepass/action/SettlementCompleteAction.java
+++ src/com/internousdev/leisurepass/action/SettlementCompleteAction.java
@@ -9,6 +9,8 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.leisurepass.dao.CartInfoDAO;
 import com.internousdev.leisurepass.dao.PurchaseHistoryInfoDAO;
+import com.internousdev.leisurepass.dto.CartInfoDTO;
+import com.internousdev.leisurepass.dto.DestinationInfoDTO;
 import com.internousdev.leisurepass.dto.PurchaseHistoryInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -17,7 +19,7 @@ public class SettlementCompleteAction extends ActionSupport implements SessionAw
 	private Map<String, Object> session;
 
 	public String execute() {
-		String result = ERROR;                                                                                //デフォルトではERRORを返す
+		String result = ERROR;                                                                              //デフォルト ERROR
 
 		@SuppressWarnings("unchecked")   //警告を抑制
 		ArrayList<PurchaseHistoryInfoDTO> purchaseHistoryInfoDtoList = (ArrayList<PurchaseHistoryInfoDTO>)session.get("purchaseHistoryInfoDtoList");   // 購入履歴の取得
@@ -26,14 +28,14 @@ public class SettlementCompleteAction extends ActionSupport implements SessionAw
 		ArrayList<DestinationInfoDTO> destinationInfoDtoList = (ArrayList<DestinationInfoDTO>)session.get("destinationInfoDtoList");                   // 宛先の取得
 
 		for(int i=0;i<purchaseHistoryInfoDtoList.size();i++) {
-			purchaseHistoryInfoDtoList.get(i).setDestinationId(destinationInfoDtoList.get(0).getId());        // 行(i)の宛先のIDを変える（宛先のリストの0番のIDに変える）
+			purchaseHistoryInfoDtoList.get(i).setDestinationId(destinationInfoDtoList.get(0).getId());      // 行(i)の宛先のIDを変える（宛先のリストの0番のIDに変える）
 		}
 
 		PurchaseHistoryInfoDAO purchaseHistoryInfoDAO = new PurchaseHistoryInfoDAO();
 		int count = 0;
 
 		for(int i=0; i<purchaseHistoryInfoDtoList.size();i++) {
-			count += purchaseHistoryInfoDAO.regist(                                                           // カウントはsqlが失敗したら0になる
+			count += purchaseHistoryInfoDAO.regist(                                                         // カウントはsqlが失敗したら0になる
 					String.valueOf(session.get("loginId")),
 					purchaseHistoryInfoDtoList.get(i).getProductId(),
 					purchaseHistoryInfoDtoList.get(i).getProductCount(),
@@ -41,17 +43,17 @@ public class SettlementCompleteAction extends ActionSupport implements SessionAw
 					purchaseHistoryInfoDtoList.get(i).getSubtotal()
 					);
 		}
-		if(count > 0) {                                                                                       // 成功した場合1
+		if(count > 0) {                                                                                     // 成功した場合1
 			CartInfoDAO cartInfoDAO = new CartInfoDAO();
-			count = cartInfoDAO.deleteAll(String.valueOf(session.get("loginId")));                            // カートの中身を削除
-			if(count > 0) {                                                                                   // 削除できたら0以上になり
-				List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();                             // 削除した後のカートの中身を取得
+			count = cartInfoDAO.deleteAll(String.valueOf(session.get("loginId")));                          // カートの中身を削除
+			if(count > 0) {                                                                                 // 削除できたら0以上になり
+				List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();                           // 削除した後のカートの中身を取得
 				cartInfoDtoList = cartInfoDAO.getCartInfoDtoList(String.valueOf(session.get("loginId")));
-				Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();                                  // iterator  リストの配列を一つづつ出力する変数を作成
-				if(!(iterator.hasNext())) {                                                                   // もうなければ
-					cartInfoDtoList = null;                                                                   // nullにする
+				Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();                                // iterator  リストの配列を一つづつ出力する変数を作成
+				if(!(iterator.hasNext())) {                                                                 // もうなければ
+					cartInfoDtoList = null;                                                                 //  nullにする
 				}
-				session.put("cartInfoDtoList", cartInfoDtoList);   //nullにしたものをセッションに入れる
+				session.put("cartInfoDtoList", cartInfoDtoList);                                            //nullにしたものをセッションに入れる
 
 				// オブジェクト型をintに変換
 				int totalPrice = Integer.parseInt(String.valueOf(cartInfoDAO.getTotalPrice(String.valueOf(session.get("loginId")))));
