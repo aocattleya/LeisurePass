495ea22d6b5585b91dcce13822ea41563d01d971
internousdev <internousdev@192.168.1.252>
2018-07-06 17:52:12 +0900

    MyPageAction訂正

--- WebContent/mypage.jsp
+++ WebContent/mypage.jsp
@@ -1,54 +1,62 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="s" uri="/struts-tags" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
 
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="./css/style.css">
-		<title>マイページ</title>
-	</head>
-	<body>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./css/style.css">
+<title>マイページ</title>
+</head>
+<body>
 
 	<jsp:include page="header.jsp" />
 	<div id="main" class="cf">
 		<jsp:include page="navigation.jsp" />
 		<div id="contents">
-		<h1>マイページ</h1>
+			<h1>マイページ</h1>
 			<s:form action="ParchaseHistoryAction">
 
-			<table class="vertical-list-table">
-			<tr>
-				<th scope="row"><s:label value="姓"/></th>
-				<td><s:property value="#session.familyName"/></td>
-			</tr>
+				<table class="vertical-list-table">
+					<tr>
+						<th scope="row"><s:label value="姓" /></th>
+						<td><s:property value="#session.familyName" /></td>
+					</tr>
 
-			<tr>
-				<th scope="row"><s:label value="名"/></th>
-				<td><s:property value="#session.firstName"/></td>
-			</tr>
+					<tr>
+						<th scope="row"><s:label value="名" /></th>
+						<td><s:property value="#session.firstName" /></td>
+					</tr>
 
-			<tr>
-				<th scope="row"><s:label value="ふりがな"/></th>
-				<td><s:property value="#session.familyNameKana"/><span>　</span><s:property value="#session.firstNameKana"/></td>
-			</tr>
+					<tr>
+						<th scope="row"><s:label value="ふりがな" /></th>
+						<td><s:property value="#session.familyNameKana" /><span>
+						</span>
+						<s:property value="#session.firstNameKana" /></td>
+					</tr>
 
-			<tr>
-				<th scope="row"><s:label value="性別"/></th>
-				<td><s:if test="#session.sex==0">男性</s:if><s:if test="#session.sex==1">女性</s:if></td>
-			</tr>
+					<tr>
+						<th scope="row"><s:label value="性別" /></th>
+						<td><s:if test="#session.sex==0">男性</s:if>
+							<s:if test="#session.sex==1">女性</s:if></td>
+					</tr>
 
-			<tr>
-				<th scope="row"><s:label value="メールアドレス"/></th>
-				<td><s:property value="#session.email"/></td>
-			</tr>
-			</table>
+					<tr>
+						<th scope="row"><s:label value="メールアドレス" /></th>
+						<td><s:property value="#session.email" /></td>
+					</tr>
 
-			<div class="submit_btn_box">
-				<s:submit value="購入履歴" class="submit_btn" />
-			</div>
+					<tr>
+						<th scope="row"><s:label value="ゆーざーID " /></th>
+						<td><s:property value="#session.userId" /></td>
+					</tr>
+				</table>
+
+				<div class="submit_btn_box">
+					<s:submit value="購入履歴" class="submit_btn" />
+				</div>
 
 			</s:form>
 		</div>
@@ -57,5 +65,5 @@
 
 	<jsp:include page="footer.jsp" />
 
-	</body>
+</body>
 </html>
--- src/com/internousdev/leisurepass/action/MyPageAction.java
+++ src/com/internousdev/leisurepass/action/MyPageAction.java
@@ -11,80 +11,71 @@ import com.internousdev.leisurepass.dto.MCategoryDTO;
 import com.internousdev.leisurepass.dto.UserInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-//	DAO,DTO未作成のためエラー
-//
-//
-
-
-public class MyPageAction extends ActionSupport implements SessionAware{
+public class MyPageAction extends ActionSupport implements SessionAware {
 
 	private String categoryId;
 	private String keywords;
-	private List<MCategoryDTO> mCategoryDTOList=new ArrayList<MCategoryDTO>();
-	private Map<String,Object> session;
+	private List<MCategoryDTO> mCategoryDTOList = new ArrayList<MCategoryDTO>();
+	private Map<String, Object> session;
 
-
-	public String execute(){
-		String result=ERROR;
+	public String execute() {
+		String result = ERROR;
 
 		System.out.println(categoryId);
 		System.out.println(keywords);
 
-		UserInfoDAO userInfoDAO=new UserInfoDAO();
-		UserInfoDTO userInfoDTO=new UserInfoDTO();
+		UserInfoDAO userInfoDAO = new UserInfoDAO();
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
 
-		userInfoDTO=userInfoDAO.getUserInfo(String.valueOf(session.get("loginId")),String.valueOf(session.get("loginPassword")));
+		userInfoDTO = userInfoDAO.getUserInfo(String.valueOf(session.get("loginId")));
 
-
-//		userInfoDTOに値があれば、セッションへ名前・性別・メールアドレスを保存する
-		if(userInfoDTO !=null){
-			session.put("familyName",userInfoDTO.getFamilyName());
+		// userInfoDTOに値があれば、セッションへ名前・性別・メールアドレスを保存する
+		if (userInfoDTO != null) {
+			session.put("familyName", userInfoDTO.getFamilyName());
 			session.put("firstName", userInfoDTO.getFirstName());
 			session.put("familyNameKana", userInfoDTO.getFamilyNameKana());
-			session.put("firstNameKana",userInfoDTO.getFirstNameKana());
-			session.put("sex",userInfoDTO.getSex());
+			session.put("firstNameKana", userInfoDTO.getFirstNameKana());
+			session.put("sex", userInfoDTO.getSex());
 			session.put("emial", userInfoDTO.getEmail());
 
 			System.out.println(session.get("familyName"));
 
-			result=SUCCESS;
+			result = SUCCESS;
 		}
 		return result;
 
 	}
 
-
-
-
-	public String getCategoryId(){
+	public String getCategoryId() {
 		return categoryId;
 	}
-	public void setCategoryId(String categoryId){
-		this.categoryId=categoryId;
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
 	}
 
-	public String getKeywords(){
+	public String getKeywords() {
 		return keywords;
 	}
-	public void setKeywords(String keywords){
-		this.keywords=keywords;
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
 	}
 
-
-	public List<MCategoryDTO> getMCategoryDTOList(){
+	public List<MCategoryDTO> getMCategoryDTOList() {
 		return mCategoryDTOList;
 	}
-	public void setMCategoryDTOList (List<MCategoryDTO> mCategoryDTOList){
-		this.mCategoryDTOList=mCategoryDTOList;
+
+	public void setMCategoryDTOList(List<MCategoryDTO> mCategoryDTOList) {
+		this.mCategoryDTOList = mCategoryDTOList;
 	}
 
-
-
-	public Map<String,Object> getSession(){
+	public Map<String, Object> getSession() {
 		return session;
 	}
+
 	@Override
-	public void setSession(Map<String,Object> session){
-		this.session=session;
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
 }
