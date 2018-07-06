aebf7a954ff59384761d876491441f1179cdbb17
internousdev <internousdev@192.168.1.252>
2018-07-06 11:24:20 +0900

    Pagination訂正

--- src/com/internousdev/leisurepass/dto/PaginationDTO.java
+++ src/com/internousdev/leisurepass/dto/PaginationDTO.java
@@ -3,167 +3,107 @@ package com.internousdev.leisurepass.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-	public class PaginationDTO {
+public class PaginationDTO {
 
-		private int totalPageSize;//	全ページ数
-		private int currentPageNo;//	現在のページ数
-		private int totalRecordSize;//	全レコード数
-		private int startRecordNo;//	開始レコード番号
-		private int endRecordNo;//		終了レコード番号
-		private List<Integer> pageNuberList=new ArrayList<Integer>();//	ページャーに表示するページ番号一覧
-		private List<ProductInfoDTO> currentProductPage;//				1ページ文のリストページ情報(商品情報)
-		private boolean hasNextPage;//			次ページが存在するか
-		private boolean hasPreviousPage;//		前ページが存在するか
-		private int nextPageNo;//				次ページ番号
-		private int previousPageNo;//			前ページ番号
+	private int totalPageSize;										// 全ページ数
+	private int currentPageNo;										// 現在のページ数
+	private int totalRecordSize;									// 全レコード数
+	private int startRecordNo;										// 開始レコード番号
+	private int endRecordNo;										// 終了レコード番号
+	private List<Integer> pageNuberList = new ArrayList<Integer>();	// ページャーに表示するページ番号一覧
+	private List<ProductInfoDTO> currentProductPage;				// 1ページ文のリストページ情報(商品情報)
+	private boolean hasNextPage;									// 次ページが存在するか
+	private boolean hasPreviousPage;								// 前ページが存在するか
+	private int nextPageNo;											// 次ページ番号
+	private int previousPageNo;										// 前ページ番号
 
+// get/setの設定
+	public int getTotalPageSize() {
+		return totalPageSize;
+	}
 
+	public void setTotalPageSize(int totalPageSize) {
+		this.totalPageSize = totalPageSize;
+	}
 
-//get/setの設定
-		public int getTotalPageSize() {
-			return totalPageSize;
-		}
-		public void setTotalPageSize(int totalPageSize) {
-			this.totalPageSize = totalPageSize;
-		}
-		public int getCurrentPageNo() {
-			return currentPageNo;
-		}
-		public void setCurrentPageNo(int currentPageNo) {
-			this.currentPageNo = currentPageNo;
-		}
-		public int getTotalRecordSize() {
-			return totalRecordSize;
-		}
-		public void setTotalRecordSize(int totalRecordSize) {
-			this.totalRecordSize = totalRecordSize;
-		}
-		public int getStartRecordNo() {
-			return startRecordNo;
-		}
-		public void setStartRecordNo(int startrecordNo) {
-			this.startRecordNo = startrecordNo;
-		}
-		public int getEndRecordNo() {
-			return endRecordNo;
-		}
-		public void setEndRecordNo(int endRecordNo) {
-			this.endRecordNo = endRecordNo;
-		}
-		public List<Integer> getPageNumberList() {
-			return pageNuberList;
-		}
-		public void setPageNuberList(List<Integer> pageNuberList) {
-			this.pageNuberList = pageNuberList;
-		}
-		public List<ProductInfoDTO> getCurrentProductPage() {
-			return currentProductPage;
-		}
-		public void setCurrentProductPage(List<ProductInfoDTO> currentProductPage) {
-			this.currentProductPage = currentProductPage;
-		}
-		public boolean hasNextPage() {
-			return hasNextPage;
-		}
-		public void setNextPage(boolean hasNextPage) {
-			this.hasNextPage = hasNextPage;
-		}
-		public boolean hasPreviousPage() {
-			return hasPreviousPage;
-		}
-		public void setPreviousPage(boolean hasPreviousPage) {
-			this.hasPreviousPage = hasPreviousPage;
-		}
-		public int getNextPageNo() {
-			return nextPageNo;
-		}
-		public void setNextPageNo(int nextPageNo) {
-			this.nextPageNo = nextPageNo;
-		}
-		public int getPreviousPageNo() {
-			return previousPageNo;
-		}
-		public void setPreviousPageNo(int previousPageNo) {
-			this.previousPageNo = previousPageNo;
-		}
+	public int getCurrentPageNo() {
+		return currentPageNo;
+	}
 
+	public void setCurrentPageNo(int currentPageNo) {
+		this.currentPageNo = currentPageNo;
+	}
 
+	public int getTotalRecordSize() {
+		return totalRecordSize;
+	}
 
+	public void setTotalRecordSize(int totalRecordSize) {
+		this.totalRecordSize = totalRecordSize;
+	}
 
+	public int getStartRecordNo() {
+		return startRecordNo;
+	}
 
+	public void setStartRecordNo(int startrecordNo) {
+		this.startRecordNo = startrecordNo;
+	}
 
+	public int getEndRecordNo() {
+		return endRecordNo;
+	}
 
+	public void setEndRecordNo(int endRecordNo) {
+		this.endRecordNo = endRecordNo;
+	}
 
+	public List<Integer> getPageNumberList() {
+		return pageNuberList;
+	}
 
+	public void setPageNuberList(List<Integer> pageNuberList) {
+		this.pageNuberList = pageNuberList;
+	}
 
+	public List<ProductInfoDTO> getCurrentProductPage() {
+		return currentProductPage;
+	}
 
-//		public int getTotalPageSize() {
-//			return totalPageSize;
-//		}
-//		public void setTotalPageSize(int totalPageSize) {
-//			this.totalPageSize = totalPageSize;
-//		}
-//		public int getCurrentPageNo() {
-//			return currentPageNo;
-//		}
-//		public void setCurrentPageNo(int currentPageNo) {
-//			this.currentPageNo = currentPageNo;
-//		}
-//		public int getTotalRecordSize() {
-//			return totalRecordSize;
-//		}
-//		public void setTotalRecordSize(int totalRecordSize) {
-//			this.totalRecordSize = totalRecordSize;
-//		}
-//		public int getStartRecordNo() {
-//			return startRecordNo;
-//		}
-//		public void setStartRecordNo(int startRecordNo) {
-//			this.startRecordNo = startRecordNo;
-//		}
-//		public int getEndRecordNo() {
-//			return endRecordNo;
-//		}
-//		public void setEndRecordNo(int endRecordNo) {
-//			this.endRecordNo = endRecordNo;
-//		}
-//		public List<Integer> getPageNumberList() {
-//			return pageNumberList;
-//		}
-//		public void setPageNumberList(List<Integer> pageNumberList) {
-//			this.pageNumberList = pageNumberList;
-//		}
-//		public List<ProductInfoDTO> getCurrentProductInfoPage() {
-//			return currentProductInfoPage;
-//		}
-//		public void setCurrentProductInfoPage(List<ProductInfoDTO> currentProductInfoPage) {
-//			this.currentProductInfoPage = currentProductInfoPage;
-//		}
-//		public boolean hasNextPage() {
-//			return hasNextPage;
-//		}
-//		public void setNextPage(boolean hasNextPage) {
-//			this.hasNextPage = hasNextPage;
-//		}
-//		public boolean hasPreviousPage() {
-//			return hasPreviousPage;
-//		}
-//		public void setPreviousPage(boolean hasPreviousPage) {
-//			this.hasPreviousPage = hasPreviousPage;
-//		}
-//		public int getNextPageNo() {
-//			return nextPageNo;
-//		}
-//		public void setNextPageNo(int nextPageNo) {
-//			this.nextPageNo = nextPageNo;
-//		}
-//		public int getPreviousPageNo() {
-//			return previousPageNo;
-//		}
-//		public void setPreviousPageNo(int previousPageNo) {
-//			this.previousPageNo = previousPageNo;
-//		}
-//	}
+	public void setCurrentProductPage(List<ProductInfoDTO> currentProductPage) {
+		this.currentProductPage = currentProductPage;
+	}
 
+	public boolean hasNextPage() {
+		return hasNextPage;
+	}
+
+	public void setNextPage(boolean hasNextPage) {
+		this.hasNextPage = hasNextPage;
+	}
+
+	public boolean hasPreviousPage() {
+		return hasPreviousPage;
+	}
+
+	public void setPreviousPage(boolean hasPreviousPage) {
+		this.hasPreviousPage = hasPreviousPage;
+	}
+
+	public int getNextPageNo() {
+		return nextPageNo;
+	}
+
+	public void setNextPageNo(int nextPageNo) {
+		this.nextPageNo = nextPageNo;
+	}
+
+	public int getPreviousPageNo() {
+		return previousPageNo;
+	}
+
+	public void setPreviousPageNo(int previousPageNo) {
+		this.previousPageNo = previousPageNo;
+	}
 
 }
--- src/com/internousdev/leisurepass/util/Pagination.java
+++ src/com/internousdev/leisurepass/util/Pagination.java
@@ -8,42 +8,80 @@ import com.internousdev.leisurepass.dto.ProductInfoDTO;
 
 public class Pagination {
 
-	public PaginationDTO initialize(List<ProductInfoDTO> list,int pageSize){
+	public PaginationDTO initialize(List<ProductInfoDTO> list, int pageSize) {
 
 		PaginationDTO pageDTO = new PaginationDTO();
-		pageDTO.setTotalPageSize((int)(Math.ceil(list.size() / pageSize)));//	全ページ数
-		pageDTO.setCurrentPageNo(1);//											現在のページ番号
-		pageDTO.setTotalRecordSize(list.size() - 1);//							全レコード数
-		pageDTO.setStartRecordNo(pageSize * (pageDTO.getCurrentPageNo() - 1));//現在のページ番号に対する開始レコード番号(オフセット)
-		pageDTO.setEndRecordNo(pageDTO.getStartRecordNo() + (pageSize - 1));//	現在のページ番号に対する終了レコード番号
+		pageDTO.setTotalPageSize((int) (Math.ceil(list.size() / pageSize)));	// 全ページ数
+		pageDTO.setCurrentPageNo(1);											// 現在のページ番号
+		pageDTO.setTotalRecordSize(list.size() - 1);							// 全レコード数
+		pageDTO.setStartRecordNo(pageSize * (pageDTO.getCurrentPageNo() - 1));	// 現在のページ番号に対する開始レコード番号(オフセット)
+		pageDTO.setEndRecordNo(pageDTO.getStartRecordNo() + (pageSize - 1));	// 現在のページ番号に対する終了レコード番号
 
-		List<Integer> pageNumberList=new ArrayList<Integer>();
-		for(int pageNumber = 1; pageNumber <= pageDTO.getTotalPageSize(); pageNumber++){
+		List<Integer> pageNumberList = new ArrayList<Integer>();
+		for (int pageNumber = 1; pageNumber <= pageDTO.getTotalPageSize(); pageNumber++) {
 			pageNumberList.add(pageNumber);
 		}
 
 		List<ProductInfoDTO> productPages = new ArrayList<ProductInfoDTO>();
-		for(int pageNumberOffset=pageDTO.getStartRecordNo(); pageNumberOffset <= pageDTO.getEndRecordNo(); pageNumberOffset++){
+		for (int pageNumberOffset = pageDTO.getStartRecordNo(); pageNumberOffset <= pageDTO
+				.getEndRecordNo(); pageNumberOffset++) {
 			productPages.add(list.get(pageNumberOffset));
 		}
 		pageDTO.setCurrentProductPage(productPages);
 
-
-		if( (pageDTO.getStartRecordNo()-1)<=0){
+		if ((pageDTO.getStartRecordNo() - 1) <= 0) {
 			pageDTO.setPreviousPage(false);
-		}else{
+		} else {
 			pageDTO.setPreviousPage(true);
-			pageDTO.setPreviousPageNo(pageDTO.getCurrentPageNo()-1);
+			pageDTO.setPreviousPageNo(pageDTO.getCurrentPageNo() - 1);
 		}
 
-		if(pageDTO.getEndRecordNo()+pageSize > pageDTO.getTotalRecordSize()){
+		if (pageDTO.getEndRecordNo() + pageSize > pageDTO.getTotalRecordSize()) {
 			pageDTO.setNextPage(false);
-		}else{
+		} else {
 			pageDTO.setNextPage(true);
-			pageDTO.setNextPageNo(pageDTO.getCurrentPageNo()+1);
+			pageDTO.setNextPageNo(pageDTO.getCurrentPageNo() + 1);
 		}
 		return pageDTO;
-
 	}
 
+	public PaginationDTO getPage(List<ProductInfoDTO> list, int pageSize, String pageNo) {
+
+		PaginationDTO paginationDTO = new PaginationDTO();										// 全ページ数
+		paginationDTO.setTotalPageSize((int) (Math.ceil(list.size() / pageSize)));				// 現在のページ番号
+		paginationDTO.setCurrentPageNo(Integer.parseInt(pageNo));								// 全レコード数
+		paginationDTO.setTotalRecordSize(list.size() - 1);										// 現在のページ番号に対する開始レコード番号（オフセット）
+		paginationDTO.setStartRecordNo(pageSize * (paginationDTO.getCurrentPageNo() - 1));		// 現在のページ番号に対する開始レコード番号
+		// paginationDTO.setStartRecordNo((pageSize * paginationDTO.getCurrentPageNo()) + 1);	// 現在のページ番号に対する終了レコード番号
+		paginationDTO.setEndRecordNo(paginationDTO.getStartRecordNo() + (pageSize - 1));
+
+		List<Integer> pageNumberList = new ArrayList<Integer>();
+		for (int pageNumber = 1; pageNumber <= paginationDTO.getTotalPageSize(); pageNumber++) {
+			pageNumberList.add(pageNumber);
+		}
+
+		List<ProductInfoDTO> productInfoPages = new ArrayList<ProductInfoDTO>();
+		for (int pageNumberOffset = paginationDTO.getStartRecordNo(); pageNumberOffset <= paginationDTO
+				.getEndRecordNo(); pageNumberOffset++) {
+			productInfoPages.add(list.get(pageNumberOffset));
+		}
+		paginationDTO.setCurrentProductPage(productInfoPages);
+
+		if ((paginationDTO.getStartRecordNo() - 1) <= 0) {
+			paginationDTO.setPreviousPage(false);
+		} else {
+			paginationDTO.setPreviousPage(true);
+			paginationDTO.setPreviousPageNo(paginationDTO.getCurrentPageNo() - 1);
+		}
+
+		if (paginationDTO.getEndRecordNo() + pageSize > paginationDTO.getTotalRecordSize()) {
+			paginationDTO.setNextPage(false);
+		} else {
+			paginationDTO.setNextPage(true);
+			paginationDTO.setNextPageNo(paginationDTO.getCurrentPageNo() + 1);
+		}
+
+		return paginationDTO;
+
+	}
 }
