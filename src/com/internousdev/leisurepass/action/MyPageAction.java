4329f79c32e53fcd4b3239ae98f490bf7c69d7a5
ykyk-314 <ykym14x3tkhr@outlook.jp>
2018-07-09 11:24:53 +0900

    MyPage.jsp誤字訂正

--- src/com/internousdev/leisurepass/action/MyPageAction.java
+++ src/com/internousdev/leisurepass/action/MyPageAction.java
@@ -16,6 +16,7 @@ public class MyPageAction extends ActionSupport implements SessionAware {
 	private String categoryId;
 	private String keywords;
 	private List<MCategoryDTO> mCategoryDTOList = new ArrayList<MCategoryDTO>();
+//	private List<UserInfoDTO> userInfoDTOList = new ArrayList<UserInfoDTO>();
 	private Map<String, Object> session;
 
 	public String execute() {
@@ -38,6 +39,10 @@ public class MyPageAction extends ActionSupport implements SessionAware {
 			session.put("firstNameKana", userInfoDTO.getFirstNameKana());
 			session.put("sex", userInfoDTO.getSex());
 			session.put("email", userInfoDTO.getEmail());
+
+/*			UserInfoDAO userDAO=new UserInfoDAO();
+			userInfoDTOList=userDAO.getUserInfo(loginId);
+*/
 
 			System.out.println(session.get("familyName"));
 
@@ -79,4 +84,13 @@ public class MyPageAction extends ActionSupport implements SessionAware {
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
+
+/*
+	public List<UserInfoDTO> getUserInfoDTOList(){
+		return userInfoDTOList;
+	}
+	public void setUserInfoDTOList(List<UserInfoDTO> userInfoDTOList){
+		this.userInfoDTOList=userInfoDTOList;
+	}
+*/
 }
