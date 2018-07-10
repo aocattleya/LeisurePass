7a922bebe6a8c5284ec96f5ee7503617c6bfb4aa
aocattleya <aocattleya@yahoo.co.jp>
2018-07-10 15:53:04 +0900

    決済確認画面で送り先情報が何もない状態のエラー修正

--- src/com/internousdev/leisurepass/action/SettlementCompleteAction.java
+++ src/com/internousdev/leisurepass/action/SettlementCompleteAction.java
@@ -14,61 +14,103 @@ import com.internousdev.leisurepass.dto.DestinationInfoDTO;
 import com.internousdev.leisurepass.dto.PurchaseHistoryInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class SettlementCompleteAction extends ActionSupport implements SessionAware{
+public class SettlementCompleteAction extends ActionSupport implements SessionAware {
 
 	private Map<String, Object> session;
+	private String noDestinationMassage;
 
 	public String execute() {
-		String result = ERROR;                                                                              //デフォルト ERROR
 
-		@SuppressWarnings("unchecked")   //警告を抑制
-		ArrayList<PurchaseHistoryInfoDTO> purchaseHistoryInfoDtoList = (ArrayList<PurchaseHistoryInfoDTO>)session.get("purchaseHistoryInfoDtoList");   // 購入履歴の取得
+		// デフォルト ERROR
+		String result = ERROR;
 
-		@SuppressWarnings("unchecked")   //警告を抑制
-		ArrayList<DestinationInfoDTO> destinationInfoDtoList = (ArrayList<DestinationInfoDTO>)session.get("destinationInfoDtoList");                   // 宛先の取得
+		// 宛先が無い場合の機能
+		if (session.get("destinationInfoDtoList") == null) {
+			setNoDestinationMassage(" 新規登録をしてください");
+			return result;
+		}
 
-		for(int i=0;i<purchaseHistoryInfoDtoList.size();i++) {
-			purchaseHistoryInfoDtoList.get(i).setDestinationId(destinationInfoDtoList.get(0).getId());      // 行(i)の宛先のIDを変える（宛先のリストの0番のIDに変える）
+		// 警告を抑制
+		@SuppressWarnings("unchecked")
+		// 購入履歴の取得
+		ArrayList<PurchaseHistoryInfoDTO> purchaseHistoryInfoDtoList = (ArrayList<PurchaseHistoryInfoDTO>) session
+				.get("purchaseHistoryInfoDtoList");
+
+		// 警告を抑制
+		@SuppressWarnings("unchecked")
+		// 宛先の取得
+		ArrayList<DestinationInfoDTO> destinationInfoDtoList = (ArrayList<DestinationInfoDTO>) session
+				.get("destinationInfoDtoList");
+
+		for (int i = 0; i < purchaseHistoryInfoDtoList.size(); i++) {
+
+			// 行(i)の宛先のIDを変える（宛先のリストの0番のIDに変える）
+			purchaseHistoryInfoDtoList.get(i).setDestinationId(destinationInfoDtoList.get(0).getId());
 		}
 
 		PurchaseHistoryInfoDAO purchaseHistoryInfoDAO = new PurchaseHistoryInfoDAO();
 		int count = 0;
 
-		for(int i=0; i<purchaseHistoryInfoDtoList.size();i++) {
-			count += purchaseHistoryInfoDAO.regist(                                                         // カウントはsqlが失敗したら0になる
-					String.valueOf(session.get("loginId")),
+		for (int i = 0; i < purchaseHistoryInfoDtoList.size(); i++) {
+
+			// カウントはsqlが失敗したら0になる
+			count += purchaseHistoryInfoDAO.regist(String.valueOf(session.get("loginId")),
 					purchaseHistoryInfoDtoList.get(i).getProductId(),
 					purchaseHistoryInfoDtoList.get(i).getProductCount(),
 					purchaseHistoryInfoDtoList.get(i).getDestinationId(),
-					purchaseHistoryInfoDtoList.get(i).getSubtotal()
-					);
+					purchaseHistoryInfoDtoList.get(i).getSubtotal());
 		}
-		if(count > 0) {                                                                                     // 成功した場合1
+
+		// 成功した場合1
+		if (count > 0) {
 			CartInfoDAO cartInfoDAO = new CartInfoDAO();
-			count = cartInfoDAO.deleteAll(String.valueOf(session.get("loginId")));                          // カートの中身を削除
-			if(count > 0) {                                                                                 // 削除できたら0以上になり
-				List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();                           // 削除した後のカートの中身を取得
+
+			// カートの中身を削除
+			count = cartInfoDAO.deleteAll(String.valueOf(session.get("loginId")));
+
+			// 削除できたら0以上になり
+			if (count > 0) {
+
+				// 削除した後のカートの中身を取得
+				List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();
 				cartInfoDtoList = cartInfoDAO.getCartInfoDtoList(String.valueOf(session.get("loginId")));
-				Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();                                // iterator  リストの配列を一つづつ出力する変数を作成
-				if(!(iterator.hasNext())) {                                                                 // もうなければ
-					cartInfoDtoList = null;                                                                 //  nullにする
+
+				// iterator リストの配列を一つづつ出力する変数を作成
+				Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();
+
+				// もうなければnullにする
+				if (!(iterator.hasNext())) {
+					cartInfoDtoList = null;
 				}
-				session.put("cartInfoDtoList", cartInfoDtoList);                                            //nullにしたものをセッションに入れる
+
+				// nullにしたものをセッションに入れる
+				session.put("cartInfoDtoList", cartInfoDtoList);
 
 				// オブジェクト型をintに変換
-				int totalPrice = Integer.parseInt(String.valueOf(cartInfoDAO.getTotalPrice(String.valueOf(session.get("loginId")))));
-				session.put("totalPrice", totalPrice);  //セッションに入れる
+				int totalPrice = Integer
+						.parseInt(String.valueOf(cartInfoDAO.getTotalPrice(String.valueOf(session.get("loginId")))));
+
+				// セッションput
+				session.put("totalPrice", totalPrice);
 				result = SUCCESS;
 			}
 		}
 		return result;
 	}
 
-
 	public Map<String, Object> getSession() {
 		return session;
 	}
+
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
+	}
+
+	public String getNoDestinationMassage() {
+		return noDestinationMassage;
+	}
+
+	public void setNoDestinationMassage(String noDestinationMassage) {
+		this.noDestinationMassage = noDestinationMassage;
 	}
 }
