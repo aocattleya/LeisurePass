f483ab0dfcd46cd393b301c489bcc1cd99dd3858
negishiayaka <wayaka820@gmail.com>
2018-07-11 14:11:37 +0900

    宛先情報の文字種変更

--- src/com/internousdev/leisurepass/action/CreateDestinationConfirmAction.java
+++ src/com/internousdev/leisurepass/action/CreateDestinationConfirmAction.java
@@ -37,13 +37,13 @@ public class CreateDestinationConfirmAction extends ActionSupport implements Ses
 		String result = ERROR;
 		InputChecker inputChecker = new InputChecker();
 //		フォームの入力内容をinputCheckerを使用しチェックする。-----------------------------------------------
-		familyNameErrorMessageList = inputChecker.doCheck("姓", familyName, 1, 16, true, true, true, false, false, false, true);
-		firstNameErrorMessageList = inputChecker.doCheck("名", firstName, 1, 16, true, true, true, false, false, false, true);
+		familyNameErrorMessageList = inputChecker.doCheck("姓", familyName, 1, 16, true, true, true, false, false, false, false);
+		firstNameErrorMessageList = inputChecker.doCheck("名", firstName, 1, 16, true, true, true, false, false, false, false);
 		familyNameKanaErrorMessageList = inputChecker.doCheck("姓ふりがな", familyNameKana, 1, 16, false, false, true, false, false, false, false);
 		firstNameKanaErrorMessageList = inputChecker.doCheck("名ふりがな", firstNameKana, 1, 16, false, false, true, false, false, false, false);
-		userAddressErrorMessageList = inputChecker.doCheck("住所", userAddress, 15, 50, false, true, true, true, true, true, true);
+		userAddressErrorMessageList = inputChecker.doCheck("住所", userAddress, 15, 50, true, true, true, true, true, true, false);
 		telNumberErrorMessageList = inputChecker.doCheck("電話番号", telNumber, 10, 13, false, false, false, true, false, false, false);
-		emailErrorMessageList = inputChecker.doCheck("メールアドレス", email, 18, 32, true, false, false, false, true, false, false);
+		emailErrorMessageList = inputChecker.doCheck("メールアドレス", email, 14, 32, true, false, false, true, true, false, false);
 		if(familyNameErrorMessageList.size()==0
 		&& firstNameErrorMessageList.size()==0
 		&&	familyNameKanaErrorMessageList.size()==0
