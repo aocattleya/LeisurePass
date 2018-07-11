aad402c0234ffdc0aceaf90d3849578ae618c1a6
internousdev01 <internousdev.leisurepass@gmail.com>
2018-07-11 17:00:22 +0900

    アニメーション追加 （絵文字のバグがあるかもです）

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,12 +1,77 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="./css/style.css">
-		<meta http-equiv="refresh" content="0;URL='HomeAction'"/>
-		<title>leisurepass</title>
-	</head>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./css/style.css">
+<meta http-equiv="refresh" content="4;URL='HomeAction'" />
+<title>leisurepass</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css"
+	href="https://fonts.googleapis.com/css?family=Inconsolata|Playfair+Display|Rubik">
+<link rel="stylesheet" href="./css/index.css">
+<link rel="stylesheet" type="text/css" href="./css/indexstyle.css" />
+<link rel="stylesheet" type="text/css" href="./css/moji.css" />
+
+<meta name="description"
+	content="A simple library to take care of complex CSS animations." />
+<meta property="og:image" content="./assets/screenshot.png" />
+<canvas id="animation"></canvas>
+</head>
+
+<body background="admission-2974645_1280.jpg" bgproperties="fixed">
+
+	<header>
+		<div id="header" class="header">
+			<canvas id="animation"></canvas>
+			<h1></h1>
+		</div>
+	</header>
+
+
+
+	<div id="wrapper">
+		<div id="letters">
+			<div class="c">??</div>
+			<div class="h">L</div>
+			<div class="r">e</div>
+			<div class="r">i</div>
+			<div class="e">s</div>
+			<div class="r">u</div>
+			<div class="g">r</div>
+			<div class="r">e</div>
+			<div class="a">??</div>
+			<div class="p">p</div>
+			<div class="h">a</div>
+			<div class="e">s</div>
+			<div class="r">s</div>
+			<div class="dash">!</div>
+			<div class="j">!</div>
+			<div class="s">??</div>
+
+			<br />
+			<div class="line"></div>
+
+			<p class="description">Let's Enjoy Japan!!</p>
+		</div>
+		<div class="scroll-overlay"></div>
+	</div>
+
+	<div class="links">
+		<div class="quickstart">??OPEN SITE??</div>
+		<a href="https://github.com/christinecha/choreographer-js">
+			<button class="reference">documentation</button>
+		</a>
+	</div>
+
+	<script src="./js/choreographer.min.js"></script>
+	<script src="./js/index.js"></script>
+
+
+	<script src="./js/animu.js"></script>
+</body>
 </html>
---
+++ WebContent/js/animu.js
@@ -0,0 +1,97 @@
+(function() {
+
+    var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
+
+    // Main
+    initHeader();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: 0, y: height};
+        
+
+        largeHeader = document.getElementById('header');
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById('animation');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create particles
+        circles = [];
+        for(var x = 0; x < width*0.5; x++) {
+            var c = new Circle();
+            circles.push(c);
+        }
+        animate();
+       
+    }
+
+    // Event handling
+    function addListeners() {
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in circles) {
+                circles[i].draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    // Canvas manipulation
+    function Circle() {
+        var _this = this;
+
+        // constructor
+        (function() {
+            _this.pos = {};
+            init();
+            console.log(_this);
+        })();
+
+        function init() {
+            _this.pos.x = Math.random()*width;
+            _this.pos.y = height+Math.random()*5000
+            //数値を大きくすることで、エフェクトの透過度が減少（最大1.0）
+            _this.alpha = 0.25+Math.random()*0.3;
+            //エフェクトサイズの大きさ
+            _this.scale = 1.5+Math.random()*0.3;
+            _this.velocity = Math.random();
+        }
+
+        this.draw = function() {
+            if(_this.alpha <= 0) {
+                init();
+            }
+            _this.pos.y -= _this.velocity *15;
+            _this.alpha -= 0.0005;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.scale*10, 0, 2 * Math.PI, false);
+            //エフェクトカラーを変更↓
+            ctx.fillStyle = 'rgba(255,160,0,'+ _this.alpha+')';
+            ctx.fill();
+        };
+    }
+
+})();
