3590bd439bdb913e9668806623b7e709effa5970
negishiayaka <ayaka_r_tora46@icloud.com>
2018-07-11 15:38:01 +0900

    パスワード再設定修正

--- src/com/internousdev/leisurepass/action/ResetPasswordConfirmAction.java
+++ src/com/internousdev/leisurepass/action/ResetPasswordConfirmAction.java
@@ -35,11 +35,11 @@ public class ResetPasswordConfirmAction extends ActionSupport implements Session
 
 		InputChecker inputChecker = new InputChecker();
 
-		loginIdErrorMessageList = inputChecker.doCheck("ログインID",loginId, 1, 8, true, false, false, true, false, false, false);
-		passwordErrorMessageList = inputChecker.doCheck("現在のパスワード",password, 1, 16, true, false, false, true, false, false, false);
-		newPasswordErrorMessageList = inputChecker.doCheck("新しいパスワード",newPassword, 1, 16, true, false, false, true, false, false, false);
+		loginIdErrorMessageList = inputChecker.doCheck("ログインID",loginId, 1, 8, true, false, false, true, false, false, false, false);
+		passwordErrorMessageList = inputChecker.doCheck("現在のパスワード",password, 1, 16, true, false, false, true, false, false, false, false);
+		newPasswordErrorMessageList = inputChecker.doCheck("新しいパスワード",newPassword, 1, 16, true, false, false, true, false, false, false, false);
 		reConfirmationNewPasswordErrorMessageList = inputChecker.doCheck("新しいパスワード(再確認)", reConfirmationPassword,
-				1, 16, true, false, false, true, false, false, false);
+				1, 16, true, false, false, true, false, false, false, false);
 
 		newPasswordIncorrectErrorMessageList = inputChecker.doPasswordCheck(newPassword, reConfirmationPassword);
 
